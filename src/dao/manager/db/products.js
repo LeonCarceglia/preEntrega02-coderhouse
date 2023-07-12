fdb3064a4b42877dd0f0165b8b907d7d5877fe8e
LeonCarceglia <51706636+LeonCarceglia@users.noreply.github.com>
2023-07-12 10:52:51 -0300

    Add files via upload

--- src/dao/manager/db/products.js
+++ src/dao/manager/db/products.js
@@ -2,11 +2,14 @@ import productModel from "../../models/product.js"
 
 export default class ProductsManager{
 
-    getProducts =  (reqs) =>{
-        const { limit = 10 } = reqs
-        const { page = 1 } = reqs
-        const { sort = "desc" } = reqs // Ver si puedo hacer que no se ordenen
-        const { query = null} = reqs
+    getProducts = async (req, currentUrl) =>{
+        const { limit = 10, page = 1, sort = "desc", query = null } = req
+        const options = {
+            limit,
+            page,
+            lean: true,
+            sort: { price: sort }
+        }
         let condition = {}
         if (query == "avaiable"){
             condition = { stock: { $gt: 0 } }
@@ -14,13 +17,20 @@ export default class ProductsManager{
         else if(query != undefined){
             condition = { category: query }
         }
-        const options = {
-            limit,
-            page,
-            lean: true,
-            sort: { price: sort }
+        
+        const result = await productModel.paginate(condition, options)
+        let prevLink = null
+        if (result.hasPrevPage) {
+            prevLink = currentUrl.replace(`page=${page}`, `page=${result.prevPage}`)
         }
-       return productModel.paginate(condition, options)
+        result.prevLink = prevLink
+        let nextLink = null
+        if (result.hasNextPage) {
+            nextLink = currentUrl.replace(`page=${page}`, `page=${result.nextPage}`)
+        }
+        result.nextLink = nextLink
+
+        return result
     }
 
     getProduct = (id) =>{
